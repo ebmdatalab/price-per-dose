6cdcdaea0860ff258cad731a8ab62f7864eaac22
Seb Bacon <seb.bacon@gmail.com>
2016-12-07 15:37:53 +0000

    Fix some bugs

--- lib/price_utils.py
+++ lib/price_utils.py
@@ -4,9 +4,10 @@ import pandas as pd
 def get_savings(for_entity='', group_by='', month=''):
     assert month
     assert group_by or for_entity
+    assert group_by in ['', 'ccg', 'practice']
     restricting_condition = (
-        "AND LENGTH(RTRIM(bnf_code)) >= 15 "
-        "AND bnf_code NOT LIKE '1902%' -- 'Selective Preparations' \n")
+        "AND LENGTH(RTRIM(p.bnf_code)) >= 15 "
+        "AND p.bnf_code NOT LIKE '1902%' -- 'Selective Preparations' \n")
     limit = ''
     if len(for_entity) == 3:
         restricting_condition += 'AND pct = "%s"' % for_entity
@@ -15,6 +16,7 @@ def get_savings(for_entity='', group_by='', month=''):
         restricting_condition += 'AND practice = "%s"' % for_entity
         group_by = 'practice'
     else:
+        group_by = 'practice'
         limit = 'LIMIT 1000'
     if group_by == 'ccg':
         select = 'savings.presentations.pct AS pct,'
@@ -40,8 +42,11 @@ def get_savings(for_entity='', group_by='', month=''):
         for key, value in substitutions:
             sql = sql.replace(key, value)
         try:
-            return pd.io.gbq.read_gbq(
+            df = pd.io.gbq.read_gbq(
                 sql, project_id="ebmdatalab", verbose=False, dialect='legacy')
+            # Rename null values in category, so we can group by it
+            df.loc[df['category'].isnull(), 'category'] = 'NP8'
+            return df
         except:
             print sql
             raise
